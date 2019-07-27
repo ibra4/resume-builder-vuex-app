885f30219818b94754c108ca24299021fe73fb66
ibra4 <43925389+ibra4@users.noreply.github.com>
2019-07-27 19:35:52 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
-// module.exports = {
-//     publicPath: ''
-// }
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/resume-builder-vue-app/'
       : '/'
   }
-//   module.exports = {
-//     publicPath: ‘<my-first-project>’
-//     } 
