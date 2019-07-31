4c836b95e36a2e77e6d9733c8a2dc24709f872cb
ibra4 <43925389+ibra4@users.noreply.github.com>
2019-07-31 13:44:09 +0300

    languages form adding

--- src/components/formComponents/languageElem.vue
+++ src/components/formComponents/languageElem.vue
@@ -3,29 +3,38 @@
         
         <titleForm :title="title" :target="'languages'" />
 
-        <button @click="addLanguage" class="btn btn-primary" style="float: right; margin-right:50px;">Add Language</button>
+        <div v-for="(lang, index) in languages" :key="index">
+            <div class="row">
+                <div class="col-md-6">
+                    <form-control :targetElem="'languages|lang|' + index" :name="'language'" />
+                </div>
+                <div class="col-md-6">
+                    <form-control :targetElem="'languages|level|' + index" :name="'level'" />
+                </div>
+            </div>
+        </div>
+        <button @click="addLanguage" class="btn btn-primary" style="float: right; margin-right:50px;">Add This Language</button>
 
     </div>
 </template>
 
 <script>
 
-// import formControl from '../generalComponents/formControl.vue'
+import formControl from '../generalComponents/formControl.vue'
 import titleForm from '../generalComponents/titleForm.vue'
 
 import { mapState } from 'vuex'
 
 export default {
-    props: [
-        'title'
-    ],
+    props: ['title'],
     data: () => {
         return {
+            formControl,
             targetElem: 'languages'
         }
     },
     components: {
-        // formControl,
+        formControl,
         titleForm
     },
     methods : {
