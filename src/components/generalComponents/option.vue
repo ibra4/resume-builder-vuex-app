1f9834af1ed879acf5234c898c970be06c7706c9
JOKER <ibra4@bitbucket.org>
2019-12-07 00:23:48 +0200

    Add option input element

---
+++ src/components/generalComponents/option.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <v-select 
+        :items="items" 
+        :lebel="label"
+        item-text="type"
+        v-model="ll"
+        v-on:change="updateVar"
+        >
+        </v-select>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data: () => {
+        return {
+            ll: ''
+        }
+    },
+    props: {
+        label: String,
+        targetElem: String,
+        items: Array
+    },
+    methods: {
+        updateVar(e) {
+            this.$store.dispatch('updateVar', [this.targetElem, this.ll])
+        }
+    }
+}
+</script>
